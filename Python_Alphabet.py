1b6bff6f718e7ea1c4f4ba7c248d71f219e83227
praveen123 <gunnampraveenreddy@gmail.com>
2023-06-02 16:19:10 +0530

    check whether enter character is alphabet or not

---
+++ Python_Alphabet.py
@@ -0,0 +1,8 @@
+# Write a program to identify if the character is an alphabet or not
+
+'''take input from user'''
+char=input('Enter a character to check whether it is alphabet or not :')
+if char.isalpha():
+    print('Entered Character is alphabet')
+else:
+    print('Entered Character is Not an alphabet ')
